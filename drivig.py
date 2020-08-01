216fd14df778b3b054f91d1cb0843b5877d6c54c
John Doe <tingyilin@TYLin.local>
2020-08-01 10:15:10 +0800

    update america

--- drivig.py
+++ drivig.py
@@ -3,6 +3,11 @@ age = input('what is your age?')
 age = int(age)
 if country == 'taiwan':
 	if age >= 18:
-		print('你可以考駕照')
+		print('you can get driving test!')
 	else:
-		print('你不能考駕照')
+		print('no qualificaiton!')
+elif country == 'the US':
+	if age >= 16:
+		print('you can get driving test!')
+	else:
+		print('no qualificaiton!')
